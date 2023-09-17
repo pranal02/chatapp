3d0e5d7d658ac56231b920ba9b144fe61eaa06c5
Pranal Wankhade <pranalwankhade002@gmail.com>
2023-09-18 01:58:58 +0530

    set api

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username }, //what ever the required data
+      { headers: { "Private-Key": "3a525f12-ecbb-4038-aa8a-733deea2d00e" } } // to authenticate
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+  
+
+
+  
   return res.json({ username: username, secret: "sha256..." });
 });
 
