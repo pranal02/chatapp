205c823f55ff7913edefe80e21e61a29603b1e51
Pranal Wankhade <pranalwankhade002@gmail.com>
2023-09-18 14:23:35 +0530

    import react chat envirom=nment

--- frontendpart/src/AuthPage.jsx
+++ frontendpart/src/AuthPage.jsx
@@ -4,7 +4,12 @@ const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
-      props.onAuth({ username: value, secret: value });
+        axios.post(
+            'http://localhost:3001/authenticate',
+            {username:value}
+        )
+        .then(r=> props.onAuth({ ...r.data, secret: value }))
+        .catch(e=>console.log('error',e))
     };
   
     return (
