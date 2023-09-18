e4d4362c9235cbede7136c39e1a488311eb91890
Pranal Wankhade <pranalwankhade002@gmail.com>
2023-09-18 13:36:11 +0530

    day 1

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+const axios = require("axios");
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
@@ -17,11 +18,6 @@ app.post("/authenticate", async (req, res) => {
   } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
-  
-
-
-  
-  return res.json({ username: username, secret: "sha256..." });
-});
+ });
 
 app.listen(3001);
